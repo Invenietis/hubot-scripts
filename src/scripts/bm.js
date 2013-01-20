82564593a400d8ffcb6d66634ddbe0a8dd8bd0f1
Antoine Blanchet <antoine.blanchet@invenietis.com>
2013-01-20 18:45:35 +0100

    update bonjour madame script to handle au revoir madame and bonjour mademoiselle

--- src/scripts/bm.js
+++ src/scripts/bm.js
@@ -2,20 +2,37 @@
 //   Enjoy your day just a little more.
 //
 // Commands:
-//   hubot show me the bonjour madame- Make sure hubot still knows the rules.
+//   hubot show me bonjour madame - Show the last 'bonjour madame'.
+//   hubot show me au revoir madame - Show the last au 'revoir madame'.
+//   hubot show me bonjour mademoiselle - Show the last au 'bonjour mademoiselle'.
 //
 // Dependencies:
 //   "cheerio": ">=0.10.5"
+
+var sites = [
+	{ url : 'http://www.bonjourmadame.fr/', selector : '.photo-panel img' },
+	{ url : 'http://www.aurevoirmadame.fr/', selector : '.photo a img' },
+	{ url : 'http://www.bonjourmademoiselle.fr/', selector : '.photo a img' }
+];
+
 var cheerio = require('cheerio');
 module.exports = function(robot) {
 	
-	var handleQuery = function(msg){
-		return msg.http("http://www.bonjourmadame.fr/").get()(function(err, res, body) {
+	var processSite = function(site, msg){
+		return msg.http(site.url).get()(function(err, res, body) {
 			$ = cheerio.load(body);
-			return msg.send($('.photo-panel img').attr('src'));
+			return msg.send($(site.selector).attr('src'));
 		});
 	};
 
-	return robot.respond(/show me bonjour madame/i, handleQuery)
+	robot.respond(/show me bonjour madame/i, function(msg){
+		return processSite(sites[0], msg);
+	});
+	robot.respond(/show me au revoir madame/i, function(msg){
+		return processSite(sites[1], msg);
+	});
+	robot.respond(/show me bonjour mademoiselle/i, function(msg){
+		return processSite(sites[2], msg);
+	});
 };
 
